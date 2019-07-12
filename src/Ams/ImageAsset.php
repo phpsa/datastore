b4658786e6bdc7115c503118431915bb2e7b9d77
Craig Smith <craig.smith@customd.com>
2019-07-12 14:46:22 +1200

    he type Phpsa\Datastore\Ams\Exception was not found.

--- src/Ams/ImageAsset.php
+++ src/Ams/ImageAsset.php
@@ -1,29 +1,50 @@
 <?php
 namespace Phpsa\Datastore\Ams;
-use Phpsa\Datastore\Asset;
 
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\View;
+use Phpsa\Datastore\Asset;
 use Intervention\Image\Facades\Image;
 
 class ImageAsset extends Asset {
 
-    public $type = ImageAsset::class;
+	/**
+	 * Asset Type
+	 *
+	 * @var string
+	 */
+	public $type = ImageAsset::class;
+
+	/**
+	 * Namespace for the asset
+	 *
+	 * @var string
+	 */
 	public $namespace = 'property';
 
-	public static function getImageUrl($filename, $width = null, $height = null){
-
+	/**
+	 * Undocumented function
+	 *
+	 * @param string $filename
+	 * @param int $width
+	 * @param int $height
+	 *
+	 * @return string
+	 */
+	public static function getImageUrl(string $filename, int $width = null, int $height = null) :string
+	{
 		$imagePath = public_path().'/vendor/phpsa-datastore/';
 
-		if ($width || $height) {
-			$path = 'thumbs/' . $width .  'x' . $height  . '.' . $filename;
+		if ($width !== null || $height !== null) {
+			$path = 'thumbs/' . (int) $width .  'x' . (int) $height  . '.' . $filename;
 
 			if(!is_file($imagePath . $path)){
 				try{
-					Image::make(public_path().'/vendor/phpsa-datastore/img/' . $filename)->resize($width,$height, function ($constraint) {
-						$constraint->aspectRatio();
-					})->save($imagePath . $path);
-				}catch(Exception $e){
+					Image::make($imagePath . 'img/' . $filename)
+						->resize($width, $height, function ($constraint)
+							{
+								$constraint->aspectRatio();
+							})
+							->save($imagePath . $path);
+				}catch(\Exception $e){
 					//Silent fallback as the image does not exist!
 				}
 			}
@@ -32,12 +53,17 @@ class ImageAsset extends Asset {
 		}
 		return '/vendor/phpsa-datastore/' . $path;
 
-
-
 	}
 
-    public static function html($data) {
-
+	/**
+	 * Gets default markup for the asset
+	 *
+	 * @param array $data
+	 *
+	 * @return string
+	 */
+	public static function html($data) :string
+	{
 		return '<img src="' . self::getImageUrl($data['value']) . '" />';
     }
 
